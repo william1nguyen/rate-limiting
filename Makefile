6908a8ff0247ca75cc635a2fdeb80da89e07d9e0
william1nguyen <vietanhhd03@gmail.com>
2026-04-01 11:33:21 +0700

    chore: setup project

---
+++ Makefile
@@ -0,0 +1,6 @@
+MAIN = main.py
+
+.PHONY: dev
+
+dev:
+	uv run $(MAIN)
---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)
+    
